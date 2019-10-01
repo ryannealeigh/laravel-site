518e08de9e0f91432cf380061f2733d1fa5b050e
Ryan Nealeigh <ryan.nealeigh@liferay.cloud>
2019-10-01 12:25:51 -0700

    Added images and logo

--- public/js/app.js
+++ public/js/app.js
@@ -49507,8 +49507,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/ryan/dev/freeCodeGram/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/ryan/dev/freeCodeGram/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/ryan/dev/laravel-site/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/ryan/dev/laravel-site/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
