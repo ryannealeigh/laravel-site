70bc69d439ced6bda004c1650cd66d4d3b576fcd
Ryan Nealeigh <ryan.nealeigh@liferay.cloud>
2019-10-01 12:25:51 -0700

    Added images and logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-3 p-5">
+			<img height="150" src="https://scontent-lax3-2.cdninstagram.com/vp/b26595d1c192bfd576d9d93c76c141c4/5E17F3C8/t51.2885-19/s320x320/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com" class="rounded-circle">
+		</div>
+		<div class="col-9 pt-5">
+			<div><h1>freeCodeCamp</h1></div>
+			<div class="d-flex">
+				<div class="pr-5"><strong>153</strong> posts</div>
+				<div class="pr-5"><strong>23k</strong> followers</div>
+				<div class="pr-5"><strong>212</strong> following</div>
+			</div>
+			<div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+			<div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+			<div>
+				<a href="#">freeCodeCamp.org</a>
+			</div>
+		</div>
+	</div>
+	<div class="row pt-5">
+		<div class="col-4">
+			<img src="https://scontent-lax3-2.cdninstagram.com/vp/e46c418679cd40f928f7d35f1834ef16/5E382C06/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/69272495_103102211028780_7054459513098794513_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_cat=100" class="w-100">
+		</div>
+		<div class="col-4">
+			<img src="https://scontent-lax3-2.cdninstagram.com/vp/1d77e339a35047c4aa6ed65d43ed7e3a/5E2123B6/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/70188541_238534077124941_2455868037630971059_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_cat=109" class="w-100">
+		</div>
+		<div class="col-4">
+			<img src="https://scontent-lax3-2.cdninstagram.com/vp/c0a74705bac1c826fdd5c7e7d1d47606/5E36CC57/t51.2885-15/sh0.08/e35/c0.40.822.822a/s640x640/69968971_2305100466416943_647506101175149940_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_cat=103" class="w-100">
+		</div>
+	</div>
 </div>
 @stop
